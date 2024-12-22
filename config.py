30f40595b8e71a9dc1ef367313611e97b9d25a7b
baniiiiiiiiii <118009209+baniadamtampubolon@users.noreply.github.com>
2024-12-23 01:54:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL', 'postgresql://postgres:adam31@localhost:5432/tubes'
+        'DATABASE_URL', 'postgresql://your_username:your_password@localhost:5432/your_database' # setup your database
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
